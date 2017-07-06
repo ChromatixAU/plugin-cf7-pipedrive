eb0c5e4a21f082867949d38c77bde90799a74590
David James <david.james@chromatix.com.au>
2017-07-06 16:36:19 +1000

    Major changes to plugin to allow for usual field name values in Contact form 7, version bump

--- cf7-pipedrive.php
+++ cf7-pipedrive.php
@@ -8,20 +8,20 @@
  * @link      http://everythinghealy.com/cf7-pipedrive
  *
  * @wordpress-plugin
- * Plugin Name: 	CF7 Pipedrive Deal on Submit
- * Plugin URI: 		http://everythinghealy.com/cf7-pipedrive
- * Description: 	CF7 Pipedrive Deal on Submit is a minimal plugin that creates a Pipedrive Deal using the form information when a Contact form 7 form is successfully submitted. Enter your API key and the forms you want Deals submitted for and you're done.
- * Version: 1
- * Author: 			Lucas Healy
- * Author URI: 		http://everythinghealy.com
- * Text Domain: 	cf7-pipedrive
- * License: 		GPL-2.0+
+ * Plugin Name:   CF7 Pipedrive Deal on Submit
+ * Plugin URI:    https://github.com/quesihealy/cf7-pipedrive
+ * Description:   CF7 Pipedrive Deal on Submit is a minimal plugin that creates a Pipedrive Deal using the form information when a Contact form 7 form is successfully submitted. Enter your API key and the forms you want Deals submitted for and you're done.
+ * Version: 1.1.0
+ * Author:      Lucas Healy
+ * Author URI:    http://everythinghealy.com
+ * Text Domain:   cf7-pipedrive
+ * License:     GPL-2.0+
  * License URI:     http://www.gnu.org/licenses/gpl-2.0.txt
  */
 
 // If this file is called directly, abort.
 if ( ! defined( 'WPINC' ) ) {
-	die;
+  die;
 }
 
 require_once plugin_dir_path( __FILE__ ) . 'class-cf7-pipedrive.php';
